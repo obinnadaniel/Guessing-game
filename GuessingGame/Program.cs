aadf97c8cb0caa0651b5a37075562876cf2a5202
Obinna Daniel <obinnadaniel30@yahoo.com>
2020-05-10 22:48:10 +0100

    initial commit

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/data/cases_country.csv"; */
+            string secretWord = "stoners";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while(guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+            }
+            if (outOfGuesses)
+            {
+                Console.Write("You Lose!");
+            }
+            else
+            {
+                Console.Write("You Win!");
+            }
+           
+
+            Console.ReadLine();
+        }
+    }
+}
